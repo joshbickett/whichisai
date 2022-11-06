3ab44210ba53bb3f507c0940ec60af1b8cb33f5e
Josh Bickett <josh@bickett.net>
2022-11-06 11:32:40 -0500

    Add logic for changing button color

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from "./assets/robot.png";
 import "./App.css";
 import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  console.log("app");
   const gradient = [
     "#000000",
     "#191919",
@@ -17,7 +18,6 @@ function App() {
     "#050505",
     "#030303",
     "#010101",
-    "#000000",
   ];
 
   const updateButton = () => {
@@ -25,6 +25,34 @@ function App() {
   };
 
   const [buttonColor, setButtonColor] = useState(gradient[0]);
+
+  useEffect(() => {
+    // updateButtonColor(0, "up");
+    console.log("test");
+  }, []);
+
+  const updateButtonColor = (index, direction) => {
+    console.log("index", index);
+    console.log("direction", direction);
+    const newColor = gradient[index];
+    console.log("newColor", newColor);
+    setButtonColor(gradient[index]);
+    if (index === gradient.length - 1) {
+      console.log("updating direction to *down*");
+      direction = "down";
+    } else if (index === 0) {
+      console.log("updating direction to *up*");
+      direction = "up";
+    }
+
+    setTimeout(() => {
+      if (direction === "up") {
+        updateButtonColor(index + 1, "up");
+      } else {
+        // updateButtonColor(index - 1, "down");
+      }
+    }, 1000);
+  };
 
   return (
     <div
@@ -47,6 +75,7 @@ function App() {
       <p style={{ margin: 0, padding: 0 }}>
         Find the AI-generated image out of the images displayed
       </p>
+
       <div
         style={{
           fontSize: "50px",
@@ -55,6 +84,15 @@ function App() {
           background: buttonColor,
           color: "white",
           borderRadius: "10px",
+          cursor: "pointer",
+        }}
+        onMouseEnter={() => {
+          const button = document.getElementById("play-button");
+          button.style.scale = "1.1";
+        }}
+        onMouseLeave={() => {
+          const button = document.getElementById("play-button");
+          button.style.scale = "1";
         }}
         id="play-button"
       >
@@ -62,6 +100,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
