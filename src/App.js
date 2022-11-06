da29b2eb4162b1277b5b5562f5e562f6ea1d2a1d
Josh Bickett <josh@bickett.net>
2022-11-06 12:23:38 -0500

    Update gitIgnore and remove old array

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import "./App.css";
 import { useEffect, useState, useRef } from "react";
 
 const App = () => {
-  // const gradient = [
-  //   "#000000",
-  //   "#191919",
-  //   "#323232",
-  //   "#4c4c4c",
-  //   "#666666",
-  //   "#7f7f7f",
-  //   "#999999",
-  //   "#b2b2b2",
-  //   "#cccccc",
-  //   "#e5e5e5",
-  //   "#ffffff",
-  // ];
-  // create a much larger gradient for the color change
   const gradient = [
     "#000000",
     "#0a0a0a",
